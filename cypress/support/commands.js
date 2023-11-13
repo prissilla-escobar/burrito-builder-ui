cc9c2bdcf30c1349b96f1364adb59c3fb3a1baa5
Prissilla Escobar <prissilla988@gmail.com>
2023-11-13 13:56:57 -0700

    Add commands for checking for buttons and cards

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('buttonSearch', (ingredient) => {
+    cy.get(`[name="${ingredient}"]`).should('have.text', ingredient)
+})
+
+Cypress.Commands.add('orderCardCheck', (index, {name, numberIng, ingNoSpacesExceptWordsWithSpace}) => {
+    cy.get('.order-card-container .order')
+      .eq(index).should('exist').as('orderCard')
+      
+
+      cy.get('@orderCard')
+        .within(() => {
+            cy.get('@orderCard')
+            .get('h3').should('have.text', name)
+            .get('.ingredient-list li')
+            .should('have.length', numberIng)
+            .get('.ingredient-list').should('have.text', ingNoSpacesExceptWordsWithSpace)
+        })
+})
