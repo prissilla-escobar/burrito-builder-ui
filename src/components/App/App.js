45b9924e685154a405bf3996515a1908c3ee49a3
Prissilla Escobar <prissilla988@gmail.com>
2023-11-13 13:56:41 -0700

    Create allOrder state, post request and pass postForm as a prop to OrderForm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getOrders } from "../../apiCalls";
 import Orders from "../../components/Orders/Orders";
 import OrderForm from "../../components/OrderForm/OrderForm";
 
 function App() {
+  const [allOrders, setAllOrders] = useState([])
+
   useEffect(() => {
-    getOrders().catch((err) => console.error("Error fetching:", err));
-  });
+    getOrders()
+      .then(data => setAllOrders(data.orders))
+      .catch((err) => console.error("Error fetching:", err));
+  }, []);
+
+  const addOrder = (newOrder) => {
+    setAllOrders([...allOrders, newOrder])
+  }
+
+  const postForm = (orderEntered) => {
+    const body = {
+      name: orderEntered.name,
+      ingredients: orderEntered.ingredients
+    }
+
+    return fetch('http://localhost:3001/api/v1/orders', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => addOrder(data))
+  }
+
 
   return (
     <main className="App">
       <header>
         <h1>Burrito Builder</h1>
-        <OrderForm />
+        <OrderForm postForm={postForm} />
       </header>
 
-      <Orders orders={"Here is where orders go"} />
+      <Orders allOrders={allOrders} />
     </main>
   );
 }
